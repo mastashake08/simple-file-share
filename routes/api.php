15938b9dd0c260dbfb2c9380bb44a35fd507ed87
Hacker God <jyrone.parker@gmail.com>
2025-04-12 16:08:14 -0400

    add API for saving files

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('/save-file', function (Request $request) {
+    $request->validate([
+        'file' => 'required|file',
+    ]);
+
+    $path = $request->file('file')->store('uploads', 'public');
+
+    // make a temporary URL and return
+    $url = Storage::temporaryUrl($path, now()->addWeek(1));
+    return response()->json(['url' => $url], 201);
+});
